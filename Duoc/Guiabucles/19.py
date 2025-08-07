d983456a1171e6169e8df7119892033c44ce6cd3
skoll43 <lukasofkonoha@gmail.com>
2025-08-06 23:15:23 -0400

    Removes all exercise files and cycle guides for cleanup
    Deletes code files, problem statements, and supporting scripts related to programming exercises and cycle guides.
    Cleans up the directory, possibly in preparation for restructuring or archiving old materials.

--- Duoc/Guiabucles/19.py
+++
@@ -1,7 +0,0 @@
-#19. Crear un programa que invierta un número entero cualquiera
-
-num = input("Ingrese un numero: ")
-numinvertido = str(num[::-1])
-print(numinvertido)
-
-
--- Duoc/Guiabucles/simpyono.py
+++
@@ -1,3 +0,0 @@
-from sympy import sieve
-primes = list(sieve.primerange(1, 10**6))
-print(primes)
